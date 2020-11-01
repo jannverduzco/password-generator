d1f25806b24a459103ad7a8026dc859ed6f25c0a
verja <janneth_verduzco@live.com>
2020-10-31 22:45:05 -0500

    created character prompt'

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@
 // variable needed to generate 
 var charString = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklnmopqrstuvwxyz", "0123456789", "~!@#$%^&*()-_=+"];
 var password = "";
+var userInput = [];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -16,45 +17,64 @@ function writePassword() {
 }
 
 // Character length prompt
-function generatePassword () {
+function generatePassword() {
   var characterLength = 0
   while ((characterLength < 8 || characterLength > 128) || Number.isInteger(characterLength) === false) {
-  characterLength - parseInt(prompt( "How many characters would you like your password to be? (8-128)"))
+    characterLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"))
+
   }
 
- charString[0] = false
- charString[1] = false
- charString[2] = false
- charString[3] = false
+  charString[0] = false
+  charString[1] = false
+  charString[2] = false
+  charString[3] = false
 
- if (!charString[0] && !charString[1] && !charString[2] &&!charString[3]){
+  while (!charString[0] && !charString[1] && !charString[2] && !charString[3]) {
 
-  
-  charString[0] = confirm("Click OK to confirm uppercase characters");
+    // Uppercase pop up
+    charString[0] = confirm("Click OK to confirm uppercase characters");
 
-  charString[1] = confirm("Click OK to confirm lowercase characters");
+    // Lowercase pop up
+    charString[1] = confirm("Click OK to confirm lowercase characters");
 
-  charString[2] = confirm("Click OK to confirm numeric characters");
+    // Numeric pop up
+    charString[2] = confirm("Click OK to confirm numeric characters");
 
-  charString[3] = confirm("Click OK to confirm special characters (~!@#$%^&*()-_=+)");
+    // Special charcter  pop up
+    charString[3] = confirm("Click OK to confirm special characters (~!@#$%^&*()-_=+)");
+
+  }
+
+  if (charString[0]) {
+    userInput += charString[0];
+
+  }
+
+  if (charString[1]) {
+    userInput += charString[1]
+
+  }
 
 
+  if (charString[2]) {
+    userInput += charString[2]
 
- }
+  }
 
+
+  if (charString[3]) {
+    userInput += charString[3]
+
+  }
+
+  for (var i=0; i< 0; i++) {
+    var index = Math.floor(Marth.Random)
+  }
 
 
 
 }
-
-
-
-
-
-
- 
-
-
+console.log(generatePassword)
 
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Password Generator
 
-## Password Generator
+## Description
+The password generator allows you to generate a password bewteen 8-128 characters. The generated characters can be symbols, numbers as well as uppercase and lowercase letters. The type and amount of characters contained is based on the informatiion obtained by the requestor. 
 
-### Description
-Password generator allows you to generate a password bewteen 8-128 characters. The generated characters can be symbols, numbers as well as uppercase and lowercase letters. The type and amount of characters contained is based on the informatiion obtained by the requestor. 
