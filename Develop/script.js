ee1eef7fa9add452575e777bdaa264be1abfbc83
verja <janneth_verduzco@live.com>
2020-10-31 23:45:49 -0500

    edit generate password

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 
 // variable needed to generate 
 var charString = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklnmopqrstuvwxyz", "0123456789", "~!@#$%^&*()-_=+"];
-var password = "";
+
 var userInput = [];
 
 var generateBtn = document.querySelector("#generate");
@@ -24,59 +24,61 @@ function generatePassword() {
 
   }
 
-  charString[0] = true
-  charString[1] = true
-  charString[2] = true
-  charString[3] = true
+  var upper = false
+  var lower = false
+  var number = false
+  var symbol = false
 
-  while (charString[0] && charString[1] && charString[2] && charString[3]) {
+  while (!upper && !lower && !number && !symbol) {
 
     // Uppercase pop up
-    charString[0] = confirm("Click OK to confirm uppercase characters");
+    upper = confirm("Click OK to confirm uppercase characters");
 
     // Lowercase pop up
-    charString[1] = confirm("Click OK to confirm lowercase characters");
+    lower = confirm("Click OK to confirm lowercase characters");
 
     // Numeric pop up
-    charString[2] = confirm("Click OK to confirm numeric characters");
+    number = confirm("Click OK to confirm numeric characters");
 
     // Special charcter  pop up
-    charString[3] = confirm("Click OK to confirm special characters (~!@#$%^&*()-_=+)");
+    symbol = confirm("Click OK to confirm special characters (~!@#$%^&*()-_=+)");
 
   }
 
-  if (charString[0]) {
+  if (upper) {
     userInput += charString[0];
 
   }
 
-  if (charString[1]) {
+  if (lower) {
     userInput += charString[1]
 
   }
 
 
-  if (charString[2]) {
+  if (number) {
     userInput += charString[2]
 
   }
 
 
-  if (charString[3]) {
+  if (symbol) {
     userInput += charString[3]
 
   }
 
-  for (var i = 0; i< 0; i++) {
-    var index = (Math.floor(Math.random) * userInput.length)
+  var password = "";
+
+  for (var i = 1; i <= userInput.length ; i++) {
+    var index = [Math.floor(Math.random) * userInput.length];
     password = password + userInput[index]
   }
+
   return password
 
 }
-console.log(generatePassword)
 
-
+console.log(returnpassword)
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Description
 The password generator allows you to generate a password bewteen 8-128 characters. The generated characters can be symbols, numbers as well as uppercase and lowercase letters. The type and amount of characters contained is based on the informatiion obtained by the requestor. 
 
+![GitHub Logo](password-generato-screenshot.PNG)
+Format: ![Alt Text](url)
